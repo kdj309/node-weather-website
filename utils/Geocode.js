92f4ce7e7c50d401dc778c2beed5086c38818ad9
kartik <karthik.joshi103@gmail.com>
2022-04-19 16:20:06 +0530

    debugging the issue

--- utils/Geocode.js
+++ utils/Geocode.js
@@ -1,8 +1,10 @@
 const axios = require("axios");
 require('dotenv').config()
 exports.geocode = (address = 'Kalaburagi', callback) => {
+    console.log(address);
     let encodedaddress = encodeURIComponent(address)
     axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedaddress}.json?limit=1&proximity=ip&types=place%2Cpostcode%2Caddress&access_token=${process.env.MAPBOX_API_KEY}`).then((result) => {
+        console.log(result);
         const data = result.data
         const latitude = data.features[0].center[1]
         const longitude = data.features[0].center[0]
@@ -14,6 +16,7 @@ exports.geocode = (address = 'Kalaburagi', callback) => {
         }
         callback(undefined, info)
     }).catch((err) => {
+        console.log(err)
         callback('please use valid location', undefined)
     });
 }
--- utils/Weather.js
+++ utils/Weather.js
@@ -2,8 +2,10 @@ const axios = require("axios")
 require('dotenv').config()
 exports.getWeatherstatus = ({ latitude, longitude, place } = {}, callback) => {
     axios.get(`http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_API_KEY}&query=${latitude.toFixed(2)},${longitude.toFixed(2)}`).then((data) => {
+        console.log(data);
         callback(undefined, { ...data.data, 'place': place })
     }).catch((err) => {
+        console.log(err)
         callback('Failed to fetch weather of this location', undefined)
     })
 }
